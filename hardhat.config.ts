ca8085484a830f9edea2be6f144a308d14f1ea1f
Evan Law <17935481+evanlaw-dev@users.noreply.github.com>
2025-08-29 13:07:40 -0400

    updated hardhard config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,40 +1,40 @@
 import type { HardhatUserConfig } from "hardhat/config";
-
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import hardhatIgnitionViem from "@nomicfoundation/hardhat-ignition-viem";
 import { configVariable } from "hardhat/config";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin],
+  plugins: [hardhatToolboxViemPlugin, hardhatIgnitionViem],
   solidity: {
     profiles: {
-      default: {
-        version: "0.8.28",
-      },
+      default: { version: "0.8.28" },
       production: {
         version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+        settings: { optimizer: { enabled: true, runs: 200 } },
       },
     },
   },
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
+    hardhatMainnet: { type: "edr-simulated", chainType: "l1" },
+    hardhatOp: { type: "edr-simulated", chainType: "op" },
     sepolia: {
       type: "http",
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      accounts: [configVariable("PRIVATE_KEY")],
+    },
+    mantaSepolia: {
+      type: "http",
+      chainType: "op", // layer 2 optimism
+      url: configVariable("MANTA_RPC_URL"),
+      accounts: [configVariable("PRIVATE_KEY")],
+      ignition: {
+        maxPriorityFeePerGas: 1_500_000_000n, // 1.5 gwei
+        maxFeePerGasLimit: 30_000_000_000n, // 30 gwei
+        disableFeeBumping: false, // so that ignition can adjust gas fees
+      },
     },
   },
 };
